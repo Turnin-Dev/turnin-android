a0dda0532b29a3166d773077d0f010b4bbf816fe
DDangDin <Kcorelec7123@gmail.com>
2025-05-19 18:41:24 +0900

    [PK-16] feat: 아이콘 추가
    - Peekr 전용 아이콘 타입 클래스 추가
    - 아이콘 리소스 추가
    - 아이콘 구조 추가

---
+++ designsystem/src/main/java/com/peekr/designsystem/util/icon/PeekrIcon.kt
@@ -0,0 +1,21 @@
+package com.peekr.designsystem.util.icon
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+
+/**
+ * Peekr 아이콘 타입
+ *
+ * @param iconRes 아이콘 리소스
+ */
+@Immutable
+class PeekrIcon(
+    @DrawableRes private val iconRes: Int,
+) {
+    val imageVector: ImageVector
+        @Composable
+        get() = ImageVector.vectorResource(iconRes)
+}
---
+++ designsystem/src/main/java/com/peekr/designsystem/util/icon/PeekrIconResources.kt
@@ -0,0 +1,183 @@
+package com.peekr.designsystem.util.icon
+
+import com.peekr.designsystem.R
+
+// ------------------------------ Filled ------------------------------
+val PeekrIcons.Filled.CircleProfile: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_circle_profile)
+
+val PeekrIcons.Filled.Home: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_home)
+
+val PeekrIcons.Filled.Profile: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_home)
+
+val PeekrIcons.Filled.People: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_people)
+
+val PeekrIcons.Filled.Like: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_like)
+
+val PeekrIcons.Filled.Discover: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_discover)
+
+val PeekrIcons.Filled.Chat: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_chat)
+
+val PeekrIcons.Filled.Pin: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_pin)
+
+val PeekrIcons.Filled.Bell: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_bell)
+
+val PeekrIcons.Filled.Notice: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_notice)
+
+val PeekrIcons.Filled.Lock: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_lock)
+
+val PeekrIcons.Filled.Report: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_report)
+
+val PeekrIcons.Filled.Block: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filled_block)
+
+// ------------------------------ Outlined ------------------------------
+val PeekrIcons.Outlined.Normal.CircleProfile: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_circle_profile_normal)
+
+val PeekrIcons.Outlined.Normal.Search: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_search_normal)
+
+val PeekrIcons.Outlined.Bold.Search: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_search_bold)
+
+val PeekrIcons.Outlined.Normal.Home: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_home_normal)
+
+val PeekrIcons.Outlined.Normal.Profile: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_profile_normal)
+
+val PeekrIcons.Outlined.Normal.People: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_people_normal)
+
+val PeekrIcons.Outlined.Normal.Like: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_like_normal)
+
+val PeekrIcons.Outlined.Normal.Discover: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_discover_normal)
+
+val PeekrIcons.Outlined.Normal.Chat: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_chat_normal)
+
+val PeekrIcons.Outlined.Normal.Pin: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_pin_normal)
+
+val PeekrIcons.Outlined.Normal.Settings: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_settings_normal)
+
+val PeekrIcons.Outlined.Bold.Settings: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_settings_bold)
+
+val PeekrIcons.Outlined.Normal.Bell: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_bell_normal)
+
+val PeekrIcons.Outlined.Bold.Bell: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_bell_bold)
+
+val PeekrIcons.Outlined.Normal.Notice: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_notice_normal)
+
+val PeekrIcons.Outlined.Normal.Lock: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_lock_normal)
+
+val PeekrIcons.Outlined.Normal.Report: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_report_normal)
+
+val PeekrIcons.Outlined.Bold.Report: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_report_bold)
+
+val PeekrIcons.Outlined.Normal.Block: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_block_normal)
+
+val PeekrIcons.Outlined.Bold.Block: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_outlined_block_bold)
+
+// ------------------------------ Default ------------------------------
+val PeekrIcons.Default.Normal.Arrow1Left: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_left_normal)
+
+val PeekrIcons.Default.Normal.Arrow1Right: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_right_normal)
+
+val PeekrIcons.Default.Normal.Arrow1Up: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_up_normal)
+
+val PeekrIcons.Default.Normal.Arrow1Down: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_down_normal)
+
+val PeekrIcons.Default.Bold.Arrow1Left: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_left_bold)
+
+val PeekrIcons.Default.Bold.Arrow1Right: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_right_bold)
+
+val PeekrIcons.Default.Bold.Arrow1Up: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_up_bold)
+
+val PeekrIcons.Default.Bold.Arrow1Down: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_1_down_bold)
+
+val PeekrIcons.Default.Normal.Arrow2Left: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_left_normal)
+
+val PeekrIcons.Default.Normal.Arrow2Right: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_right_normal)
+
+val PeekrIcons.Default.Normal.Arrow2Up: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_up_normal)
+
+val PeekrIcons.Default.Normal.Arrow2Down: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_down_normal)
+
+val PeekrIcons.Default.Bold.Arrow2Left: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_left_bold)
+
+val PeekrIcons.Default.Bold.Arrow2Right: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_right_bold)
+
+val PeekrIcons.Default.Bold.Arrow2Up: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_up_bold)
+
+val PeekrIcons.Default.Bold.Arrow2Down: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_arrow_2_down_bold)
+
+val PeekrIcons.Default.Normal.Plus: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_plus_normal)
+
+val PeekrIcons.Default.Bold.Plus: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_plus_bold)
+
+val PeekrIcons.Default.Normal.Minus: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_minus_normal)
+
+val PeekrIcons.Default.Bold.Minus: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_minus_bold)
+
+val PeekrIcons.Default.Normal.Filter: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filter_normal)
+
+val PeekrIcons.Default.Bold.Filter: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_filter_bold)
+
+val PeekrIcons.Default.Normal.Check: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_check_normal)
+
+val PeekrIcons.Default.Bold.Check: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_check_bold)
+
+val PeekrIcons.Default.Normal.Cancel: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_cancel_normal)
+
+val PeekrIcons.Default.Bold.Cancel: PeekrIcon
+    get() = PeekrIcon(R.drawable.ic_cancel_bold)
