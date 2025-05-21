403a921e0b6569263c69514ae037f8876d4ee2b9
DDangDin <Kcorelec7123@gmail.com>
2025-05-21 23:25:26 +0900

    [PK-17] build: 디자인 시스템 모듈 긴급 수정
    - 기존에 디자인 시스템이 애플리케이션 모듈로 되어있었던 문제 수정 (라이브러리 모듈로 수정)
    
    [디자인 시스템 모듈 수정 (애플리케이션 모듈 -> 라이브러리 모듈)]

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
@@ -9,13 +9,10 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.peekr.designsystem"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     testOptions {
@@ -23,19 +20,23 @@ android {
             isIncludeAndroidResources = true
         }
     }
+
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures { compose = true }
 }
 
