9e4470e8d1fef4bab658bea5d6e52202e872a8fa
DDangDin <Kcorelec7123@gmail.com>
2025-05-23 16:39:16 +0900

    [PK-17] feat: Coil 이미지 캐싱 로직 추가

---
+++ app/src/main/java/com/peekr/peekrapp/PeekrApplication.kt
@@ -0,0 +1,43 @@
+package com.peekr.peekrapp
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
+
+class PeekrApplication : Application(), ImageLoaderFactory {
+    // 약 10MB 정도
+    private val imageCacheMaxSize: Long = 10 * 1024 * 1024
+
+    // 최대 메모리의 10퍼센트
+    private val memoryCacheMaxSizePercent: Double = 0.10
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun newImageLoader(): ImageLoader = ImageLoader
+        .Builder(this)
+        .memoryCachePolicy(CachePolicy.ENABLED) // 메모리 캐시 활성화
+        .memoryCache {
+            MemoryCache
+                .Builder(this)
+                .maxSizePercent(memoryCacheMaxSizePercent)
+                .build()
+        }.diskCachePolicy(CachePolicy.ENABLED) // 디스크 캐시 활성화
+        .diskCache {
+            DiskCache
+                .Builder()
+                .maxSizeBytes(imageCacheMaxSize)
+                .directory(cacheDir.resolve("flip_image_cache"))
+                .build()
+        }.apply {
+            if (BuildConfig.DEBUG) {
+                logger(DebugLogger())
+            }
+        }.respectCacheHeaders(true) // 서버의 캐시 제어 헤더 사용 여부
+        .build()
+}
