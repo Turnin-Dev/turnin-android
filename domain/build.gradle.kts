edc28ed2130a0b753856ff633a78941e9aa483b5
DDangDin <Kcorelec7123@gmail.com>
2025-07-10 16:19:20 +0900

    [PK-30] build: domain 모듈 추가

---
+++ domain/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ksp)
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    api(libs.kotlinx.coroutines.core)
+    implementation(libs.dagger.hilt.javax)
+
+    // Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.mockK)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "Peekr"
 include(":app")
 include(":designsystem")
+include(":domain")
