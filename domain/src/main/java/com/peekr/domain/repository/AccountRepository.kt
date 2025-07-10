6c008cbdfd044ae401c0f17deae998f07a82ff00
DDangDin <Kcorelec7123@gmail.com>
2025-07-10 17:14:08 +0900

    [PK-30] feat: AccountRepository-login 추가

--- domain/src/main/java/com/peekr/domain/repository/AccountRepository.kt
+++ domain/src/main/java/com/peekr/domain/repository/AccountRepository.kt
@@ -1,8 +1,12 @@
 package com.peekr.domain.repository
 
 import com.peekr.domain.model.account.Login
+import com.peekr.domain.util.ErrorType
+import com.peekr.domain.util.Result
+import kotlinx.coroutines.flow.Flow
 
 /** 계정 관련 리포지토리 */
 interface AccountRepository {
-    suspend fun login(login: Login)
+    /** 로그인 */
+    suspend fun login(login: Login): Flow<Result<Boolean, ErrorType>>
 }
