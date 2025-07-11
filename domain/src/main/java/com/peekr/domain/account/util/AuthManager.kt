6e3697c04340122562773af50a205d384edc57c1
DDangDin <Kcorelec7123@gmail.com>
2025-07-11 16:40:51 +0900

    [PK-31] feat: AuthManager 추가

---
+++ domain/src/main/java/com/peekr/domain/account/util/AuthManager.kt
@@ -0,0 +1,17 @@
+package com.peekr.domain.account.util
+
+import com.peekr.domain.account.model.Login
+import com.peekr.domain.shared.util.ErrorType
+import com.peekr.domain.shared.util.Result
+
+/** 소셜로그인 인증을 관리한다. */
+interface AuthManager {
+    /** 해당하는 소셜로그인 플랫폼에 로그인 한다. */
+    suspend fun signIn(): Result<Login, ErrorType>
+
+    /** 해당하는 소셜로그인 플랫폼을 로그아웃 한다. */
+    suspend fun signOut(): Result<Boolean, ErrorType>
+
+    /** 해당하는 소셜로그인 플랫폼에서 계정을 삭제한다. */
+    suspend fun deleteAccount(): Result<Boolean, ErrorType>
+}
