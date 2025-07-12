dac56f6887d0ba2a6ad9959b1a62b0128db060b7
DDangDin <Kcorelec7123@gmail.com>
2025-07-12 23:13:54 +0900

    [PK-31] refactor: AuthManager 메서드 반환 타입 전부 Flow로 래핑

--- domain/src/main/java/com/peekr/domain/account/util/AuthManager.kt
+++ domain/src/main/java/com/peekr/domain/account/util/AuthManager.kt
@@ -3,15 +3,16 @@ package com.peekr.domain.account.util
 import com.peekr.domain.account.model.UserUID
 import com.peekr.domain.shared.util.ErrorType
 import com.peekr.domain.shared.util.Result
+import kotlinx.coroutines.flow.Flow
 
 /** 소셜로그인 인증을 관리한다. */
 interface AuthManager {
     /** 해당하는 소셜로그인 플랫폼에 로그인 한다. */
-    suspend fun signIn(): Result<UserUID, ErrorType>
+    suspend fun signIn(): Flow<Result<UserUID, ErrorType>>
 
     /** 해당하는 소셜로그인 플랫폼을 로그아웃 한다. */
-    suspend fun signOut(): Result<Unit, ErrorType>
+    suspend fun signOut(): Flow<Result<Unit, ErrorType>>
 
     /** 해당하는 소셜로그인 플랫폼에서 계정을 삭제한다. */
-    suspend fun deleteAccount(): Result<Unit, ErrorType>
+    suspend fun deleteAccount(): Flow<Result<Unit, ErrorType>>
 }
