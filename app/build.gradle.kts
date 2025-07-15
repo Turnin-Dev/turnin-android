9c3346eb557e67e328405de340221cd4db45d5c5
DDangDin <Kcorelec7123@gmail.com>
2025-07-15 17:22:28 +0900

    [PK-31] build: app 모듈에 hilt 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.gms)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -66,6 +68,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt Dependency Injection
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     // Coil
     implementation(libs.coil.compose)
