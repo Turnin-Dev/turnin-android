f0803b79dc2c1874dd518a33829ee03cf48b8e6b
DDangDin <Kcorelec7123@gmail.com>
2025-07-15 18:29:34 +0900

    [PK-31] feat: AccountApi 추가

---
+++ data/src/main/java/com/peekr/data/shared/util/NetworkApiPath.kt
@@ -0,0 +1,8 @@
+package com.peekr.data.shared.util
+
+object NetworkApiPath {
+    private const val VERSION = "/v1"
+    private const val BASE = "/api$VERSION"
+
+    const val AUTH = "$BASE/auth"
+}
