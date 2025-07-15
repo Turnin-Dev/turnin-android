64696c5bb2011181c8fad295f02bf28968337c51
DDangDin <Kcorelec7123@gmail.com>
2025-07-15 18:32:58 +0900

    [PK-31] refactor: Moshi 어노테이션 추가

--- data/src/main/java/com/peekr/data/account/model/request/LoginRequest.kt
+++ data/src/main/java/com/peekr/data/account/model/request/LoginRequest.kt
@@ -1,11 +1,14 @@
 package com.peekr.data.account.model.request
 
+import com.squareup.moshi.JsonClass
+
 /**
  * 로그인 요청 바디
  *
  * @property provider 소셜로그인 플랫폼
  * @property providerId 소셜로그인 플랫폼에서 제공된 id
  */
+@JsonClass(generateAdapter = true)
 data class LoginRequest(
     val provider: String,
     val providerId: String,
