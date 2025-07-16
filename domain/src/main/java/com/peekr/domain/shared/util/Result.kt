68c94944360d4e81051ba14301e6b6df551e389f
DDangDin <Kcorelec7123@gmail.com>
2025-07-16 20:29:57 +0900

    [PK-31] refactor: Error 타입 수정

--- domain/src/main/java/com/peekr/domain/shared/util/Result.kt
+++ domain/src/main/java/com/peekr/domain/shared/util/Result.kt
@@ -7,19 +7,20 @@ sealed interface Result<out T, out E : BaseError> {
     /**
      * 성공 시
      *
-     * @param data 성공 후 반환할 데이터
+     * @property data 성공 후 반환할 데이터
      */
     data class Success<out T>(val data: T) : Result<T, Nothing>
 
     /**
      * 실패 시
      *
-     * @param error 에러 타입
-     * @param message 에러 메시지
+     * @property error 에러 타입
+     * @property message 에러 메시지
+     * @property detail 에러 부가 설명
      */
     data class Error<out E : BaseError>(
         val error: E,
         val message: String? = null,
-        val errorBody: ErrorBody? = null,
+        val detail: String? = null,
     ) : Result<Nothing, E>
 }
