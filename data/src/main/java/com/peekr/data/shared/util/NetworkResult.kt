416148dc38d29a23c99324ff62af663129aa0d88
DDangDin <Kcorelec7123@gmail.com>
2025-07-16 20:31:02 +0900

    [PK-31] feat: NetworkResult & NetworkErrorType 추가

---
+++ data/src/main/java/com/peekr/data/shared/util/NetworkResult.kt
@@ -0,0 +1,28 @@
+package com.peekr.data.shared.util
+
+import com.peekr.data.shared.util.network.NetworkErrorType
+
+/** 네트워크 호출 결과 래퍼 클래스 */
+sealed interface NetworkResult<out T> {
+    /**
+     * 네트워크 호출 성공 시
+     *
+     * @property data 성공 후 반환할 데이터
+     */
+    data class Success<out T>(val data: T) : NetworkResult<T>
+
+    /**
+     * 네트워크 호출 성공 시
+     *
+     * @property error 에러 타입
+     * @property code 에러 코드
+     * @property status 에러 상태 코드
+     * @property message 에러 메시지
+     */
+    data class Error(
+        val error: NetworkErrorType,
+        val code: String? = null,
+        val status: Int? = null,
+        val message: String? = null,
+    ) : NetworkResult<Nothing>
+}
---
+++ data/src/main/java/com/peekr/data/shared/util/network/NetworkErrorType.kt
@@ -0,0 +1,28 @@
+package com.peekr.data.shared.util.network
+
+sealed interface NetworkErrorType {
+    /** Network 에러 타입 */
+    enum class Network : NetworkErrorType {
+        EmptyResponse,
+        BadRequest,
+        Unauthorized,
+        Forbidden,
+        NotFound,
+        RequestTimeout,
+        InternalServerError,
+        BadGateway,
+        ServiceUnavailable,
+        HttpError,
+    }
+
+    /** Exception 에러 타입 */
+    enum class Exception : NetworkErrorType {
+        HTTP,
+        IO,
+        TIMEOUT,
+        JSON_DATA,
+        JSON_ENCODING,
+        MALFORMED_JSON,
+        Unexpected,
+    }
+}
