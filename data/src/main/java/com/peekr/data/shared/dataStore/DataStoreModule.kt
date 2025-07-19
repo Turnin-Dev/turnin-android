2f2c1f0afbf5edb842216b69a60bbb6952a70e76
DDangDin <Kcorelec7123@gmail.com>
2025-07-20 00:30:41 +0900

    feat: DataStoreModule 추가

---
+++ data/src/main/java/com/peekr/data/shared/dataStore/DataStoreModule.kt
@@ -0,0 +1,26 @@
+package com.peekr.data.shared.dataStore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.peekr.data.BuildConfig
+import com.peekr.domain.shared.dataStore.DataStoreManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = BuildConfig.PEEKR_DATA_STORE)
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataStoreModule {
+    @Singleton
+    @Provides
+    fun provideDataStoreManager(
+        @ApplicationContext context: Context,
+    ): DataStoreManager = DataStoreManagerImpl(context.dataStore)
+}
