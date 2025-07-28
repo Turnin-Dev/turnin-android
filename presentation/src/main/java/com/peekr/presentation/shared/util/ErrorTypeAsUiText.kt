0dc26797edacde743b36962cc4c9539298813bcb
DDangDin <Kcorelec7123@gmail.com>
2025-07-28 23:50:27 +0900

    [PK-32] feat: UI에서 사용하는 UiText 추가

---
+++ presentation/src/main/java/com/peekr/presentation/shared/util/ErrorTypeAsUiText.kt
@@ -0,0 +1,28 @@
+package com.peekr.presentation.shared.util
+
+import com.peekr.domain.shared.util.ErrorType
+import com.peekr.presentation.R
+import com.peekr.presentation.shared.util.UiText.StringResource
+
+fun ErrorType.asUiText(): UiText = when (this) {
+    // ------------------------------ Auth ------------------------------
+    ErrorType.Auth.IdTokenParsing -> StringResource(R.string.error_auth_id_token_parsing)
+    ErrorType.Auth.Cancellation -> StringResource(R.string.error_auth_cancellation)
+    ErrorType.Auth.TokenTypeInvalid -> StringResource(R.string.error_auth_token_type_invalid)
+    ErrorType.Auth.UserNotFound -> StringResource(R.string.error_auth_user_not_found)
+    ErrorType.Auth.DeleteAccountFailed -> StringResource(R.string.error_auth_delete_account_failed)
+    ErrorType.Auth.KakaoSignInError -> StringResource(R.string.error_auth_kakao_sign_in_error)
+    ErrorType.Auth.KakaoSignOutError -> StringResource(R.string.error_auth_kakao_sign_out_error)
+    ErrorType.Auth.KakaoDeleteAccountError -> StringResource(R.string.error_auth_kakao_delete_account_error)
+    ErrorType.Auth.Unexpected -> StringResource(R.string.error_auth_unexpected)
+    // ------------------------------ Exception ------------------------------
+    ErrorType.Exception.Json -> StringResource(R.string.error_exception_json)
+    ErrorType.Exception.TimeOut -> StringResource(R.string.error_exception_timeout)
+    ErrorType.Exception.IO -> StringResource(R.string.error_exception_io)
+    ErrorType.Exception.Unexpected -> StringResource(R.string.error_exception_unexpected)
+    // ------------------------------ Network ------------------------------
+    ErrorType.Network.Unauthorized -> StringResource(R.string.error_network_unauthorized)
+    ErrorType.Network.ClientError -> StringResource(R.string.error_network_client)
+    ErrorType.Network.ServerError -> StringResource(R.string.error_network_server)
+    ErrorType.Network.Unexpected -> StringResource(R.string.error_network_unexpected)
+}
