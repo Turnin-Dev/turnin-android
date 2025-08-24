e273456076bae2e9cb7fe8098e0ea173d46e6d60
DDangDin <Kcorelec7123@gmail.com>
2025-08-24 22:27:34 +0900

    [PK-37] chore: 테스트 코드 위치 수정

--- domain/src/test/kotlin/com/peekr/domain/account/usecase/login/LoginIntegrationUseCaseTest.kt
+++ domain/src/test/kotlin/com/peekr/domain/account/usecase/login/LoginIntegrationUseCaseTest.kt
@@ -1,11 +1,10 @@
-package com.peekr.domain.account.usecase
+package com.peekr.domain.account.usecase.login
 
 import com.peekr.domain.account.model.JWTToken
 import com.peekr.domain.account.model.Login
 import com.peekr.domain.account.model.ProviderId
 import com.peekr.domain.account.model.SocialLoginProvider
-import com.peekr.domain.account.usecase.login.LoginIntegrationUseCase
-import com.peekr.domain.account.usecase.login.LoginUseCase
+import com.peekr.domain.account.usecase.SaveRefreshTokenUseCase
 import com.peekr.domain.shared.util.ErrorType
 import com.peekr.domain.shared.util.Result
 import io.mockk.every
