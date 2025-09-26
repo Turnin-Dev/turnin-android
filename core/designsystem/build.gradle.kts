6e56d6117bea340366864f952d4c58fe83acc799
DDangDin <Kcorelec7123@gmail.com>
2025-09-26 15:33:50 +0900

    refactor: :core:designsystem 빌드 구성 수정

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,44 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.peekr.android.library)
+    alias(libs.plugins.peekr.android.library.compose)
 }
 
 android {
     namespace = "com.peekr.core.designsystem"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildFeatures { compose = true }
 }
 
 kotlin {
@@ -49,14 +15,10 @@ dependencies {
     // Essential
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-
-    // Compose
-    val composeBom = platform(libs.androidx.compose.bom)
-    implementation(composeBom)
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.util)
+    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
-    debugImplementation(libs.androidx.ui.tooling)
 
     // Coil
     implementation(libs.coil.compose)
@@ -66,7 +28,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
-    androidTestImplementation(composeBom)
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
