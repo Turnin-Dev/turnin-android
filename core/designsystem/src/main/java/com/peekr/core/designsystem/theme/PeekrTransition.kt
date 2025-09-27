03fdf21815dc42cbd545ac7be2be704a243e552b
DDangDin <Kcorelec7123@gmail.com>
2025-09-27 22:40:20 +0900

    [PK-46] refactor: PeekrModal 삭제 후 PeekrModalWrapper 추가 & 트랜지션 애니메이션 수정

--- core/designsystem/src/main/java/com/peekr/core/designsystem/theme/PeekrTransition.kt
+++ core/designsystem/src/main/java/com/peekr/core/designsystem/theme/PeekrTransition.kt
@@ -2,14 +2,12 @@ package com.peekr.core.designsystem.theme
 
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
-import androidx.compose.animation.core.EaseInBack
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
@@ -18,8 +16,8 @@ import androidx.compose.animation.slideOutVertically
 data class PeekrTransition(
     val fadeIn: EnterTransition = fadeIn(),
     val fadeOut: ExitTransition = fadeOut(),
-    val dialogEnter: EnterTransition = AnimationTokens.fadeInSpring + AnimationTokens.scaleInSpring,
-    val dialogExit: ExitTransition = AnimationTokens.fadeOutTween + AnimationTokens.scaleOutTween,
+    val dialogEnter: EnterTransition = AnimationTokens.slideInVerticallyTween + AnimationTokens.fadeInTween,
+    val dialogExit: ExitTransition = AnimationTokens.slideOutVerticallyTween + AnimationTokens.fadeOutTween,
 ) {
     fun slideInTransition(direction: PeekrTransitionDirection): EnterTransition =
         when (direction) {
@@ -106,20 +104,38 @@ enum class PeekrTransitionDirection {
 }
 
 private object AnimationTokens {
-    val fadeInSpring = fadeIn(spring(stiffness = Spring.StiffnessHigh))
-    val scaleInSpring = scaleIn(
-        initialScale = .8f,
-        animationSpec =
-            spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessMediumLow,
-            ),
+    // ------------------------------ V2 ------------------------------
+    val fadeInTween = fadeIn(tween(durationMillis = 120, easing = FastOutSlowInEasing))
+    val slideInVerticallyTween = slideInVertically(
+        initialOffsetY = { 32 },
+        animationSpec = tween(
+            durationMillis = 200,
+            easing = CubicBezierEasing(0.25f, 0.46f, 0.45f, 0.94f),
+        ),
     )
-    val fadeOutTween = fadeOut(tween(easing = EaseInBack))
-    val scaleOutTween = scaleOut(
-        targetScale = .8f,
-        animationSpec = tween(easing = EaseInBack),
+    val fadeOutTween = fadeOut(tween(durationMillis = 100, easing = FastOutLinearInEasing))
+    val slideOutVerticallyTween = slideOutVertically(
+        targetOffsetY = { 24 },
+        animationSpec = tween(
+            durationMillis = 150,
+            easing = CubicBezierEasing(0.55f, 0.06f, 0.68f, 0.19f),
+        ),
     )
+
+    // ------------------------------ V1 ------------------------------
+//    val fadeInSpring = fadeIn(spring(stiffness = Spring.StiffnessHigh))
+//    val scaleInSpring = scaleIn(
+//        initialScale = .9f,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessMediumLow,
+//        ),
+//    )
+//    val fadeOutTween = fadeOut(tween(easing = EaseInBack))
+//    val scaleOutTween = scaleOut(
+//        targetScale = .9f,
+//        animationSpec = tween(easing = EaseInBack),
+//    )
 }
 
 private const val SLIDE_ANIMATION_DELAY = 100
