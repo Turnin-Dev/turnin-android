b905f7e9ccf0e5516ebbf0e32ed5c83954ff6693
DDangDin <Kcorelec7123@gmail.com>
2025-10-22 23:51:08 +0900

    refactor: Result, BaseError 대규모 리팩토링 - Keyword

---
+++ core/domain/src/main/java/com/peekr/core/domain/keyword/error/KeywordErrorType.kt
@@ -0,0 +1,12 @@
+package com.peekr.core.domain.keyword.error
+
+import com.peekr.core.domain.util.BaseError
+import com.peekr.core.domain.util.CommonErrorType
+
+sealed interface KeywordErrorType : BaseError {
+    /** 알 수 없는 에러로 자세한 사항은 [cause] 파라미터에 [Throwable]형태로 담는다. */
+    data class Unexpected(val cause: Throwable?) : KeywordErrorType
+
+    // ------------------------------ Other Error Type ------------------------------
+    data class CommonError(val error: CommonErrorType) : KeywordErrorType
+}
