068507e051aeb3614149f742e7bccbbc5727fbf5
DDangDin <Kcorelec7123@gmail.com>
2025-10-23 20:38:55 +0900

    refactor: Result, BaseError 대규모 리팩토링 - 누락된 수정 파일

--- core/data/src/main/java/com/peekr/core/data/userKeyword/repository/UserKeywordRepositoryImpl.kt
+++ core/data/src/main/java/com/peekr/core/data/userKeyword/repository/UserKeywordRepositoryImpl.kt
@@ -8,6 +8,7 @@ import com.peekr.core.data.userKeyword.network.request.toDataModel
 import com.peekr.core.data.userKeyword.network.response.toDomainModel
 import com.peekr.core.domain.coroutine.safeResultFlow
 import com.peekr.core.domain.model.UserKeywordId
+import com.peekr.core.domain.userKeyword.error.UserKeywordErrorType
 import com.peekr.core.domain.userKeyword.model.CreateUserKeyword
 import com.peekr.core.domain.userKeyword.model.PatchUserKeyword
 import com.peekr.core.domain.userKeyword.model.UserKeyword
@@ -22,8 +23,11 @@ class UserKeywordRepositoryImpl @Inject constructor(
     private val userKeywordDataSource: UserKeywordDataSource,
     @IO private val ioDispatcher: CoroutineDispatcher,
 ) : UserKeywordRepository {
-    override fun getUserKeywords(): Flow<Result<UserKeywords, ErrorType>> =
-        safeResultFlow(ioDispatcher) {
+    override fun getUserKeywords(): Flow<Result<UserKeywords, UserKeywordErrorType>> =
+        safeResultFlow<UserKeywords, UserKeywordErrorType>(
+            dispatcher = ioDispatcher,
+            unexpectedErrorMapper = { UserKeywordErrorType.Unexpected(it) },
+        ) {
             emit(Result.Loading)
 
             when (val result = userKeywordDataSource.getUserKeywords()) {
@@ -32,13 +36,17 @@ class UserKeywordRepositoryImpl @Inject constructor(
                 }
 
                 is NetworkResult.Error -> {
-                    emit(Result.Error(error = result.error.toCommonErrorType(), message = result.message))
+                    val error = UserKeywordErrorType.CommonError(result.error.toCommonErrorType())
+                    emit(Result.Error(error = error, message = result.message))
                 }
             }
         }
 
-    override fun createUserKeyword(create: CreateUserKeyword): Flow<Result<UserKeyword, ErrorType>> =
-        safeResultFlow(ioDispatcher) {
+    override fun createUserKeyword(create: CreateUserKeyword): Flow<Result<UserKeyword, UserKeywordErrorType>> =
+        safeResultFlow<UserKeyword, UserKeywordErrorType>(
+            dispatcher = ioDispatcher,
+            unexpectedErrorMapper = { UserKeywordErrorType.Unexpected(it) },
+        ) {
             emit(Result.Loading)
 
             when (val result = userKeywordDataSource.createUserKeyword(create.toDataModel())) {
@@ -47,7 +55,8 @@ class UserKeywordRepositoryImpl @Inject constructor(
                 }
 
                 is NetworkResult.Error -> {
-                    emit(Result.Error(error = result.error.toCommonErrorType(), message = result.message))
+                    val error = UserKeywordErrorType.CommonError(result.error.toCommonErrorType())
+                    emit(Result.Error(error = error, message = result.message))
                 }
             }
         }
@@ -55,8 +64,11 @@ class UserKeywordRepositoryImpl @Inject constructor(
     override fun patchUserKeyword(
         userKeywordId: UserKeywordId,
         patch: PatchUserKeyword,
-    ): Flow<Result<Unit, ErrorType>> =
-        safeResultFlow(ioDispatcher) {
+    ): Flow<Result<Unit, UserKeywordErrorType>> =
+        safeResultFlow<Unit, UserKeywordErrorType>(
+            dispatcher = ioDispatcher,
+            unexpectedErrorMapper = { UserKeywordErrorType.Unexpected(it) },
+        ) {
             emit(Result.Loading)
 
             when (
@@ -70,22 +82,27 @@ class UserKeywordRepositoryImpl @Inject constructor(
                 }
 
                 is NetworkResult.Error -> {
-                    emit(Result.Error(error = result.error.toCommonErrorType(), message = result.message))
+                    val error = UserKeywordErrorType.CommonError(result.error.toCommonErrorType())
+                    emit(Result.Error(error = error, message = result.message))
                 }
             }
         }
 
     override fun deleteUserKeyword(
         userKeywordId: UserKeywordId,
-    ): Flow<Result<Unit, ErrorType>> =
-        safeResultFlow(ioDispatcher) {
+    ): Flow<Result<Unit, UserKeywordErrorType>> =
+        safeResultFlow<Unit, UserKeywordErrorType>(
+            dispatcher = ioDispatcher,
+            unexpectedErrorMapper = { UserKeywordErrorType.Unexpected(it) },
+        ) {
             when (val result = userKeywordDataSource.deleteUserKeyword(userKeywordId)) {
                 is NetworkResult.Success -> {
                     emit(Result.Success(Unit))
                 }
 
                 is NetworkResult.Error -> {
-                    emit(Result.Error(error = result.error.toCommonErrorType(), message = result.message))
+                    val error = UserKeywordErrorType.CommonError(result.error.toCommonErrorType())
+                    emit(Result.Error(error = error, message = result.message))
                 }
             }
         }
