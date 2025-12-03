3ab9b994246e0a98dec9d5db5306f116cad776e2
DDangDin <Kcorelec7123@gmail.com>
2025-12-04 00:52:40 +0900

    [PK-55] test: 신고 데이터 소스, 리포지토리 테스트 코드 추가

---
+++ core/data/src/test/java/com/peekr/core/data/repository/ReportRepositoryImplTest.kt
@@ -0,0 +1,165 @@
+package com.peekr.core.data.repository
+
+import com.peekr.core.data.source.network.datasource.ReportNetworkDataSource
+import com.peekr.core.data.source.network.dto.report.response.ReportReasonResponse
+import com.peekr.core.data.source.network.dto.report.response.ReportReasonsResponse
+import com.peekr.core.data.source.network.dto.report.response.toDomainModel
+import com.peekr.core.data.source.network.error.NetworkErrorType
+import com.peekr.core.data.source.network.error.toCommonErrorType
+import com.peekr.core.data.source.network.util.NetworkResult
+import com.peekr.core.domain.common.CommonErrorType
+import com.peekr.core.domain.common.Result
+import com.peekr.core.domain.model.UserId
+import com.peekr.core.domain.report.model.Report
+import com.peekr.core.domain.report.model.ReportReasonId
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ReportRepositoryImplTest {
+    private val dataSource: ReportNetworkDataSource = mockk()
+    private val dispatcher = UnconfinedTestDispatcher()
+    private val repository = ReportRepositoryImpl(dataSource, dispatcher)
+
+    @Test
+    fun `신고 사유 목록 조회 - 성공 테스트`() = runTest {
+        // given
+        coEvery {
+            dataSource.getReportReasons()
+        } returns NetworkResult.Success(TestReportReasonsResponse)
+
+        // when
+        val result = repository.getReportReasons().last()
+
+        // then
+        val success = result as Result.Success
+        assertEquals(
+            TestReportReasonsResponse.toDomainModel(),
+            success.data,
+        )
+    }
+
+    @Test
+    fun `신고 사유 목록 조회 - 알려진 에러 방출 시 정상적으로 에러를 반환한다`() = runTest {
+        // given
+        val expectedError = NetworkErrorType.Network.Forbidden
+        coEvery {
+            dataSource.getReportReasons()
+        } returns NetworkResult.Error(expectedError)
+
+        // when
+        val result = repository.getReportReasons().last()
+
+        // then
+        val error = result as Result.Error
+        assertEquals(
+            expectedError.toCommonErrorType(),
+            error.error,
+        )
+    }
+
+    @Test
+    fun `신고 사유 목록 조회 - 예외 발생 시 정상적으로 에러를 반환한다`() = runTest {
+        // given
+        val expectedException = Exception("error!")
+        coEvery {
+            dataSource.getReportReasons()
+        } throws expectedException
+
+        // when
+        val result = repository.getReportReasons().last()
+
+        // then
+        assertTrue(result is Result.Error)
+        if (result is Result.Error && result.error is CommonErrorType.Unexpected) {
+            assertEquals(
+                CommonErrorType.Unexpected(expectedException).cause?.message,
+                (result.error as CommonErrorType.Unexpected).cause?.message,
+            )
+        }
+    }
+
+    @Test
+    fun `신고 생성 - 성공 테스트`() = runTest {
+        // given
+        coEvery {
+            dataSource.createReport(any())
+        } returns NetworkResult.Success(Unit)
+
+        // when
+        val result = repository.createReport(TestReport).last()
+
+        // then
+        assertTrue(result is Result.Success)
+    }
+
+    @Test
+    fun `신고 생성 - 알려진 에러 방출 시 정상적으로 에러를 반환한다`() = runTest {
+        // given
+        val expectedError = NetworkErrorType.Network.Forbidden
+        coEvery {
+            dataSource.createReport(any())
+        } returns NetworkResult.Error(expectedError)
+
+        // when
+        val result = repository.createReport(TestReport).last()
+
+        // then
+        val error = result as Result.Error
+        assertEquals(
+            expectedError.toCommonErrorType(),
+            error.error,
+        )
+    }
+
+    @Test
+    fun `신고 생성 - 예외 발생 시 정상적으로 에러를 반환한다`() = runTest {
+        // given
+        val expectedException = Exception("error!")
+        coEvery {
+            dataSource.createReport(any())
+        } throws expectedException
+
+        // when
+        val result = repository.createReport(TestReport).last()
+
+        // then
+        assertTrue(result is Result.Error)
+        if (result is Result.Error && result.error is CommonErrorType.Unexpected) {
+            assertEquals(
+                CommonErrorType.Unexpected(expectedException).cause?.message,
+                (result.error as CommonErrorType.Unexpected).cause?.message,
+            )
+        }
+    }
+
+    companion object {
+        private val TestReportReasonResponse = ReportReasonResponse(
+            id = 1L,
+            code = "SPAM",
+            description = "스팸 및 사기",
+        )
+        private val TestReportReasonsResponse = ReportReasonsResponse(
+            reasons = listOf(TestReportReasonResponse),
+        )
+        private val TestInvalidResponse =
+            """
+            {
+                "what": "???"
+            }
+            """.trimIndent()
+        private val TestReport = Report(
+            reporterId = UserId(1L),
+            reportedId = UserId(2L),
+            reasonId = ReportReasonId(1L),
+            customReason = "reason",
+        )
+    }
+}
---
+++ core/data/src/test/java/com/peekr/core/data/source/network/datasource/ReportNetworkDataSourceImplTest.kt
@@ -0,0 +1,194 @@
+package com.peekr.core.data.source.network.datasource
+
+import com.peekr.core.data.ServerTestRule
+import com.peekr.core.data.source.network.api.ReportApi
+import com.peekr.core.data.source.network.dto.report.request.ReportRequest
+import com.peekr.core.data.source.network.dto.report.response.ReportReasonResponse
+import com.peekr.core.data.source.network.dto.report.response.ReportReasonsResponse
+import com.peekr.core.data.source.network.error.NetworkErrorType
+import com.peekr.core.data.source.network.util.NetworkResult
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import okhttp3.mockwebserver.MockResponse
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ReportNetworkDataSourceImplTest {
+    @get:Rule
+    val testRule = ServerTestRule()
+
+    private val reportApi: ReportApi
+        get() = testRule.createNetworkApi<ReportApi>(testRule.moshi)
+
+    private lateinit var dataSource: ReportNetworkDataSource
+
+    @Before
+    fun setUp() {
+        dataSource = ReportNetworkDataSourceImpl(reportApi)
+    }
+
+    @Test
+    fun `신고 사유 목록 조회 - 성공 테스트`() = runTest {
+        // given
+        val testResponseJson = testRule.encodeToJson(TestReportReasonsResponse)
+        testRule.server.enqueue(
+            MockResponse().apply {
+                setResponseCode(200)
+                setBody(testResponseJson)
+            },
+        )
+
+        // when
+        val response = dataSource.getReportReasons()
+
+        // then
+        val success = response as NetworkResult.Success
+        assertEquals(
+            TestReportReasonsResponse.reasons.size,
+            success.data.reasons.size,
+        )
+        assertEquals(
+            TestReportReasonsResponse.reasons.first().description,
+            success.data.reasons.first().description,
+        )
+    }
+
+    @Test
+    fun `신고 사유 목록 조회 - 잘못된 응답 바디로 응답 시 알려진 에러를 반환한다`() = runTest {
+        // given
+        testRule.server.enqueue(
+            MockResponse().apply {
+                setResponseCode(200)
+                setBody(TestInvalidResponse)
+            },
+        )
+
+        // when
+        val response = dataSource.getReportReasons()
+
+        // then
+        val error = response as NetworkResult.Error
+        assertEquals(NetworkErrorType.Exception.JsonData, error.error)
+    }
+
+    @Test
+    fun `신고 사유 목록 조회 - 알 수 없는 예외 발생 시 Unexpected 에러를 반환한다`() = runTest {
+        // given
+        val mockApi: ReportApi = mockk()
+        val exception = Exception()
+        dataSource = ReportNetworkDataSourceImpl(mockApi)
+        coEvery { mockApi.getReportReasons() } throws exception
+
+        // when
+        val response = dataSource.getReportReasons()
+
+        // then
+        assertTrue(response is NetworkResult.Error)
+        assertEquals(
+            NetworkErrorType.Unexpected(exception),
+            (response as NetworkResult.Error).error,
+        )
+    }
+
+    @Test
+    fun `신고 사유 목록 조회 - HTTP 상태코드 404 응답 시 NotFound 에러로 반환한다`() = runTest {
+        // given
+        testRule.server.enqueue(
+            MockResponse().apply {
+                setResponseCode(404)
+            },
+        )
+
+        // when
+        val response = dataSource.getReportReasons()
+
+        // then
+        assertTrue(response is NetworkResult.Error)
+        assertEquals(
+            NetworkErrorType.Network.NotFound,
+            (response as NetworkResult.Error).error,
+        )
+    }
+
+    @Test
+    fun `신고 생성 - 성공 테스트`() = runTest {
+        // given
+        testRule.server.enqueue(
+            MockResponse().apply {
+                setResponseCode(200)
+            },
+        )
+
+        // when
+        val response = dataSource.createReport(TestReportRequest)
+
+        // then
+        assertTrue(response is NetworkResult.Success)
+    }
+
+    @Test
+    fun `신고 생성 - 알 수 없는 예외 발생 시 Unexpected 에러를 반환한다`() = runTest {
+        // given
+        val mockApi: ReportApi = mockk()
+        val exception = Exception()
+        dataSource = ReportNetworkDataSourceImpl(mockApi)
+        coEvery { mockApi.createReport(any()) } throws exception
+
+        // when
+        val response = dataSource.createReport(TestReportRequest)
+
+        // then
+        assertTrue(response is NetworkResult.Error)
+        assertEquals(
+            NetworkErrorType.Unexpected(exception),
+            (response as NetworkResult.Error).error,
+        )
+    }
+
+    @Test
+    fun `신고 생성 - HTTP 상태코드 404 응답 시 NotFound 에러로 반환한다`() = runTest {
+        // given
+        testRule.server.enqueue(
+            MockResponse().apply {
+                setResponseCode(404)
+            },
+        )
+
+        // when
+        val response = dataSource.createReport(TestReportRequest)
+
+        // then
+        assertTrue(response is NetworkResult.Error)
+        assertEquals(
+            NetworkErrorType.Network.NotFound,
+            (response as NetworkResult.Error).error,
+        )
+    }
+
+    companion object {
+        private val TestReportReasonResponse = ReportReasonResponse(
+            id = 1L,
+            code = "SPAM",
+            description = "스팸 및 사기",
+        )
+        private val TestReportReasonsResponse = ReportReasonsResponse(
+            reasons = listOf(TestReportReasonResponse),
+        )
+        private val TestInvalidResponse =
+            """
+            {
+                "what": "???"
+            }
+            """.trimIndent()
+        private val TestReportRequest = ReportRequest(
+            reporterId = 1L,
+            reportedId = 2L,
+            reasonId = 1L,
+            customReason = "reason",
+        )
+    }
+}
