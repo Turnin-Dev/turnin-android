f2f8c68712ceedc9f7ded046eea95e4406c12163
DDangDin <Kcorelec7123@gmail.com>
2025-12-06 22:14:19 +0900

    [PK-61] test: 유스케이스 테스트 코드 추가

--- app/src/main/java/com/peekr/peekrapp/navigation/BottomNavigation.kt
+++ app/src/main/java/com/peekr/peekrapp/navigation/BottomNavigation.kt
@@ -8,7 +8,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navigation
 import com.peekr.core.presentation.common.navigation.Screens
 import com.peekr.core.presentation.common.navigation.SubGraph
 import com.peekr.core.presentation.common.navigation.bottom.BottomNavigationFrame
