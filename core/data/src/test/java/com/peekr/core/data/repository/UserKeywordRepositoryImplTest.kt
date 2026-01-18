5fc7a450914ef31716001c05a90b1b96120bf06e
DDangDin <Kcorelec7123@gmail.com>
2026-01-18 22:03:07 +0900

    [PK-94] test: 키워드 상세 정보 조회 기능 테스트 코드 추가

--- core/data/src/test/java/com/peekr/core/data/repository/UserKeywordRepositoryImplTest.kt
+++ core/data/src/test/java/com/peekr/core/data/repository/UserKeywordRepositoryImplTest.kt
@@ -1,8 +1,13 @@
 package com.peekr.core.data.repository
 
 import com.peekr.core.data.source.local.database.dao.MyKeywordDao
+import com.peekr.core.data.source.local.database.dao.MyProfileDao
+import com.peekr.core.data.source.local.database.dao.UserKeywordDetailDao
 import com.peekr.core.data.source.local.database.entity.MyKeywordEntity
+import com.peekr.core.data.source.local.database.entity.MyProfileEntity
+import com.peekr.core.data.source.local.database.entity.UserKeywordDetailEntity
 import com.peekr.core.data.source.local.database.entity.toDomainModel
+import com.peekr.core.data.source.local.datastore.DataStoreManager
 import com.peekr.core.data.source.local.memory.MemoryCache
 import com.peekr.core.data.source.network.datasource.UserKeywordNetworkDataSource
 import com.peekr.core.data.source.network.datasource.UserNetworkDataSource
@@ -14,7 +19,6 @@ import com.peekr.core.data.source.network.dto.userKeyword.request.PatchDescripti
 import com.peekr.core.data.source.network.dto.userKeyword.response.DescriptionResponse
 import com.peekr.core.data.source.network.dto.userKeyword.response.PatchDescriptionResponse
 import com.peekr.core.data.source.network.dto.userKeyword.response.UserKeywordResponse
-import com.peekr.core.data.source.network.dto.userKeyword.response.UserKeywordsResponse
 import com.peekr.core.data.source.network.dto.userKeyword.response.toDomainModel
 import com.peekr.core.data.source.network.error.NetworkErrorType
 import com.peekr.core.data.source.network.error.toCommonErrorType
@@ -31,14 +35,18 @@ import com.peekr.core.domain.userKeyword.model.PatchDescription
 import com.peekr.core.domain.userKeyword.model.UserKeywordDetail
 import io.mockk.Runs
 import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -47,23 +55,64 @@ class UserKeywordRepositoryImplTest {
     private val userKeywordNetworkDataSource: UserKeywordNetworkDataSource = mockk()
     private val userNetworkDataSource: UserNetworkDataSource = mockk()
     private val myKeywordDao: MyKeywordDao = mockk()
-    private val memoryCache: MemoryCache<Long, List<UserKeywordDetail>> = mockk()
+    private val myProfileDao: MyProfileDao = mockk()
+    private val userKeywordDetailDao: UserKeywordDetailDao = mockk()
+    private val memoryListCache: MemoryCache<UserId, List<UserKeywordDetail>> = mockk()
+    private val memoryCache: MemoryCache<UserKeywordId, UserKeywordDetail> = mockk()
+    private val dataStoreManager: DataStoreManager = mockk()
     private val dispatcher = UnconfinedTestDispatcher()
     private val repository = UserKeywordRepositoryImpl(
-        userKeywordNetworkDataSource,
-        userNetworkDataSource,
-        myKeywordDao,
-        memoryCache,
-        dispatcher,
+        userKeywordNetworkDataSource = userKeywordNetworkDataSource,
+        userNetworkDataSource = userNetworkDataSource,
+        myKeywordDao = myKeywordDao,
+        myProfileDao = myProfileDao,
+        userKeywordDetailDao = userKeywordDetailDao,
+        memoryListCache = memoryListCache,
+        memoryCache = memoryCache,
+        dataStoreManager = dataStoreManager,
+        ioDispatcher = dispatcher,
     )
 
+    // ------------------------------ getMyDetailFromLocal() ------------------------------
     @Test
-    fun `키워드 상세 정보 조회 - 성공 테스트(캐시 X)`() = runTest {
+    fun `나의 키워드 상세 정보 로컬 조회 - 성공 테스트(DB 데이터 O)`() = runTest {
+        // given
+        coEvery { dataStoreManager.getLongData(any()) } returns flowOf(TestUserId.value)
+        coEvery { myProfileDao.getByUserId(TestUserId.value) } returns flowOf(TestMyProfileEntity)
+        coEvery { myKeywordDao.getById(TestUserKeywordId.value) } returns flowOf(TestMyKeywordEntity)
+
+        // when
+        val result = repository.getMyDetailFromLocal(TestUserKeywordId).last()
+
+        // then
+        assertEquals(TestUserKeywordDetailResponse.toDomainModel(), result)
+    }
+
+    @Test
+    fun `나의 키워드 상세 정보 로컬 조회 - 성공 테스트(DB 데이터 X)`() = runTest {
+        // given
+        coEvery { dataStoreManager.getLongData(any()) } returns flowOf(TestUserId.value)
+        coEvery { myProfileDao.getByUserId(TestUserId.value) } returns flowOf(null)
+        coEvery { myKeywordDao.getById(TestUserKeywordId.value) } returns flowOf(null)
+
+        // when
+        val result = repository.getMyDetailFromLocal(TestUserKeywordId).last()
+
+        // then
+        assertNull(result)
+    }
+
+    // ------------------------------ getDetail() ------------------------------
+
+    @Test
+    fun `키워드 상세 정보 조회 - 성공 테스트(메모리 캐시 X, DB 데이터 X)`() = runTest {
         // given
         coEvery {
             userKeywordNetworkDataSource.getDetail(TestUserKeywordId)
         } returns NetworkResult.Success(TestUserKeywordDetailResponse)
-        coEvery { memoryCache[TestUserId.value] } returns null
+        every { memoryCache[TestUserKeywordId] } returns null
+        coEvery { userKeywordDetailDao.getById(TestUserKeywordId.value) } returns null
+        every { memoryCache[TestUserKeywordId] = any() } just Runs
 
         // when
         val result = repository.getDetail(TestUserId, TestUserKeywordId).last()
@@ -74,14 +123,40 @@ class UserKeywordRepositoryImplTest {
     }
 
     @Test
-    fun `키워드 상세 정보 조회 - 성공 테스트(캐시 O)`() = runTest {
+    fun `키워드 상세 정보 조회 - 성공 테스트(DB 데이터 O)`() = runTest {
         // given
         coEvery {
             userKeywordNetworkDataSource.getDetail(TestUserKeywordId)
         } returns NetworkResult.Success(TestUserKeywordDetailResponse)
+        every { memoryCache[TestUserKeywordId] } returns null
+        every { memoryCache[TestUserKeywordId] = any() } just Runs
         coEvery {
-            memoryCache[TestUserId.value]
-        } returns listOf(TestUserKeywordDetailResponse.toDomainModel())
+            userKeywordDetailDao.getById(TestUserKeywordId.value)
+        } returns TestUserKeywordDetailEntity
+
+        // when
+        val result = repository.getDetail(TestUserId, TestUserKeywordId).last()
+
+        // then
+        val success = result as Result.Success
+        assertEquals(TestUserKeywordDetailResponse.toDomainModel(), success.data)
+        // then: 해당 데이터가 메모리 캐시에도 승격됐는지 검증
+        verify {
+            memoryCache[TestUserKeywordId]
+        }
+    }
+
+    @Test
+    fun `키워드 상세 정보 조회 - 성공 테스트(메모리 캐시 O)`() = runTest {
+        // given
+        coEvery {
+            userKeywordNetworkDataSource.getDetail(TestUserKeywordId)
+        } returns NetworkResult.Success(TestUserKeywordDetailResponse)
+        every { memoryCache[TestUserKeywordId] = any() } just Runs
+        coEvery { userKeywordDetailDao.getById(TestUserKeywordId.value) } returns null
+        every {
+            memoryCache[TestUserKeywordId]
+        } returns TestUserKeywordDetailResponse.toDomainModel()
 
         // when
         val result = repository.getDetail(TestUserId, TestUserKeywordId).last()
@@ -90,6 +165,49 @@ class UserKeywordRepositoryImplTest {
         val success = result as Result.Success
         assertEquals(TestUserKeywordDetailResponse.toDomainModel(), success.data)
     }
+
+    // ------------------------------ getDetailRefresh() ------------------------------
+
+    @Test
+    fun `키워드 상세 정보 새로고침 - 성공 테스트(내 키워드인 경우)`() = runTest {
+        // given
+        coEvery { dataStoreManager.getLongData(any()) } returns flowOf(TestUserId.value)
+        coEvery {
+            userKeywordNetworkDataSource.getDetail(any())
+        } returns NetworkResult.Success(TestUserKeywordDetailResponse)
+        coEvery { myKeywordDao.upsert(any()) } just Runs
+
+        // when
+        val result = repository.getDetailRefresh(TestUserId, TestUserKeywordId).last()
+
+        // then
+        val success = result as Result.Success
+        assertEquals(TestUserKeywordDetailResponse.toDomainModel(), success.data)
+        // 내 키워드인 경우 DB에 업데이트했는지 검증
+        coVerify { myKeywordDao.upsert(any()) }
+    }
+
+    @Test
+    fun `키워드 상세 정보 새로고침 - 성공 테스트(내 키워드가 아닌 경우)`() = runTest {
+        // given
+        val otherUserId = 100L
+        coEvery { dataStoreManager.getLongData(any()) } returns flowOf(otherUserId)
+        coEvery {
+            userKeywordNetworkDataSource.getDetail(any())
+        } returns NetworkResult.Success(TestUserKeywordDetailResponse)
+        coEvery { memoryCache[any()] = any() } just Runs
+
+        // when
+        val result = repository.getDetailRefresh(TestUserId, TestUserKeywordId).last()
+
+        // then
+        val success = result as Result.Success
+        assertEquals(TestUserKeywordDetailResponse.toDomainModel(), success.data)
+        // 내 키워드가 아닌 경우 메모리 캐시에 업데이트했는지 검증
+        coVerify { memoryCache[any()] = any() }
+    }
+
+    // ------------------------------ getMyKeywords() ------------------------------
 
     @Test
     fun `나의 키워드 상세 정보 리스트 조회 - 성공 테스트`() = runTest {
@@ -107,6 +225,8 @@ class UserKeywordRepositoryImplTest {
         assertEquals(expectedCount, result.size)
         assertEquals(expectedList.map { it.toDomainModel() }, result)
     }
+
+    // ------------------------------ getMyKeywordsRefresh() ------------------------------
 
     @Test
     fun `나의 키워드 상세 정보 리스트 새로고침 - 성공 테스트`() = runTest {
@@ -127,6 +247,8 @@ class UserKeywordRepositoryImplTest {
         assertTrue(result is Result.Success)
     }
 
+    // ------------------------------ getUserKeywords() ------------------------------
+
     @Test
     fun `사용자 키워드 상세 정보 리스트 조회 - 성공 테스트(메모리 캐시 X)`() = runTest {
         // given
@@ -135,8 +257,9 @@ class UserKeywordRepositoryImplTest {
         coEvery {
             userNetworkDataSource.getUserKeywords(any())
         } returns NetworkResult.Success(expectedList)
-        coEvery { memoryCache[TestUserId.value] } returns null
-        coEvery { memoryCache[TestUserId.value] = any() } returns Unit
+        coEvery { memoryListCache[TestUserId] } returns null
+        coEvery { memoryListCache[TestUserId] = any() } returns Unit
+        coEvery { memoryCache[any()] = any() } returns Unit
 
         // when
         val result = repository.getUserKeywords(TestUserId).last()
@@ -156,7 +279,7 @@ class UserKeywordRepositoryImplTest {
             userNetworkDataSource.getUserKeywords(any())
         } returns NetworkResult.Success(expectedList)
         coEvery {
-            memoryCache[TestUserId.value]
+            memoryListCache[TestUserId]
         } returns expectedList.map { it.toDomainModel() }
 
         // when
@@ -168,63 +291,7 @@ class UserKeywordRepositoryImplTest {
         assertEquals(expectedList.map { it.toDomainModel() }, success.data)
     }
 
-    @Test
-    fun `사용자 키워드 설명 조회 - 성공 테스트`() = runTest {
-        // given
-        coEvery {
-            userKeywordNetworkDataSource.getDescription(TestUserKeywordId)
-        } returns NetworkResult.Success(TestDescriptionResponse)
-
-        // when
-        val result = repository.getDescription(TestUserKeywordId).last()
-
-        // then
-        assertTrue(result is Result.Success)
-        assertEquals(
-            TestDescriptionResponse.toDomainModel(),
-            (result as Result.Success).data,
-        )
-    }
-
-    @Test
-    fun `사용자 키워드 설명 조회 - 알려진 에러 방출 시 정상적으로 에러를 반환한다`() = runTest {
-        // given
-        val expectedError = NetworkErrorType.Unexpected(null)
-        coEvery {
-            userKeywordNetworkDataSource.getDescription(TestUserKeywordId)
-        } returns NetworkResult.Error(expectedError)
-
-        // when
-        val result = repository.getDescription(TestUserKeywordId).last()
-
-        // then
-        assertTrue(result is Result.Error)
-        assertEquals(
-            expectedError.toCommonErrorType(),
-            (result as Result.Error).error,
-        )
-    }
-
-    @Test
-    fun `사용자 키워드 설명 조회 - 예외 발생 시 정상적으로 에러를 반환한다`() = runTest {
-        // given
-        val exception = Exception("error!")
-        coEvery {
-            userKeywordNetworkDataSource.getDescription(TestUserKeywordId)
-        } throws exception
-
-        // when
-        val result = repository.getDescription(TestUserKeywordId).last()
-
-        // then
-        assertTrue(result is Result.Error)
-        if (result is Result.Error && result.error is CommonErrorType.Unexpected) {
-            assertEquals(
-                CommonErrorType.Unexpected(exception).cause?.message,
-                (result.error as CommonErrorType.Unexpected).cause?.message,
-            )
-        }
-    }
+    // ------------------------------ createUserKeyword() ------------------------------
 
     @Test
     fun `사용자 키워드 생성 - 성공 테스트`() = runTest {
@@ -292,6 +359,8 @@ class UserKeywordRepositoryImplTest {
             )
         }
     }
+
+    // ------------------------------ patchDescription() ------------------------------
 
     @Test
     fun `사용자 키워드 설명 수정 - 성공 테스트`() = runTest {
@@ -381,6 +450,8 @@ class UserKeywordRepositoryImplTest {
         }
     }
 
+    // ------------------------------ deleteUserKeyword() ------------------------------
+
     @Test
     fun `사용자 키워드 삭제 - 성공 테스트`() = runTest {
         // given
@@ -458,9 +529,6 @@ class UserKeywordRepositoryImplTest {
             createdAt = 1000,
             updatedAt = 1000,
         )
-        private val TestUserKeywordsResponse = UserKeywordsResponse(
-            keywords = listOf(TestUserKeywordResponse),
-        )
         private val TestCreateUserKeywordRequest = CreateUserKeywordRequest(
             userId = TestUserId.value,
             keyword = TestKeyword.value,
@@ -501,6 +569,27 @@ class UserKeywordRepositoryImplTest {
             ),
             createdAt = 1000L,
             updatedAt = 1000L,
+        )
+        private val TestUserKeywordDetailEntity = UserKeywordDetailEntity(
+            userKeywordId = TestUserKeywordDetailResponse.userKeywordId,
+            keywordId = TestUserKeywordDetailResponse.keywordId,
+            keywordName = TestUserKeywordDetailResponse.keywordName,
+            description = TestUserKeywordDetailResponse.description,
+            userId = TestUserKeywordDetailResponse.userInfo.userId,
+            userName = TestUserKeywordDetailResponse.userInfo.userName,
+            profileImageUrl = TestUserKeywordDetailResponse.userInfo.profileImageUrl,
+            createdAt = TestUserKeywordDetailResponse.createdAt,
+            updatedAt = TestUserKeywordDetailResponse.updatedAt,
+        )
+        private val TestMyProfileEntity = MyProfileEntity(
+            userId = TestUserId.value,
+            displayId = "did",
+            name = "name",
+            profileImageUrl = null,
+            introduce = "introduce",
+            lastLoginAt = 1000L,
+            friendsCount = 10,
+            active = true,
         )
     }
 }
