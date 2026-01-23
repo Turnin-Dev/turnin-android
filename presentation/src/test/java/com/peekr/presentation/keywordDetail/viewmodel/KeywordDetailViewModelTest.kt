0a20a5b06f545a8f38ee3b6ec72a59824b50f11a
DDangDin <Kcorelec7123@gmail.com>
2026-01-23 19:56:57 +0900

    [PK-94] test: KeywordDetailViewModel 테스트 코드 추가 및 수정

--- presentation/src/test/java/com/peekr/presentation/keywordDetail/viewmodel/KeywordDetailViewModelTest.kt
+++ presentation/src/test/java/com/peekr/presentation/keywordDetail/viewmodel/KeywordDetailViewModelTest.kt
@@ -3,22 +3,20 @@ package com.peekr.presentation.keywordDetail.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import com.peekr.core.domain.common.Result
 import com.peekr.core.domain.model.KeywordDescription
-import com.peekr.core.domain.model.KeywordDescription.Companion.invoke
 import com.peekr.core.domain.model.KeywordId
-import com.peekr.core.domain.model.KeywordId.Companion.invoke
 import com.peekr.core.domain.model.KeywordName
-import com.peekr.core.domain.model.KeywordName.Companion.invoke
 import com.peekr.core.domain.model.Name
-import com.peekr.core.domain.model.Name.Companion.invoke
 import com.peekr.core.domain.model.UserId
 import com.peekr.core.domain.model.UserKeywordId
 import com.peekr.core.domain.user.usecase.GetUserIdUseCase
 import com.peekr.core.presentation.FakeSnackbarController
 import com.peekr.core.presentation.MVIBaseViewModelTest
 import com.peekr.core.presentation.common.snackbar.SnackbarEvent
+import com.peekr.core.presentation.ui.util.UiText
 import com.peekr.domain.keywordDetail.error.KeywordDetailErrorType
 import com.peekr.domain.keywordDetail.model.KeywordDetail
 import com.peekr.domain.keywordDetail.usecase.KeywordDetailUseCases
+import com.peekr.presentation.R
 import com.peekr.presentation.keywordDetail.error.asUiText
 import com.peekr.presentation.keywordDetail.model.toUiModel
 import com.peekr.presentation.keywordDetail.state.KeywordDetailContract
@@ -48,14 +46,180 @@ class KeywordDetailViewModelTest : MVIBaseViewModelTest<
 
     @Before
     fun setUp() {
-        // 현재 사용자 ID는 나의 사용자 ID로 고정 해놓고 테스트 진행
-        coEvery { getUserIdUseCase() } returns TestUserId
+        // 나의 키워드인 경우를 테스트하기 위해 나의 UserId로 설정한다.
+        coEvery { getUserIdUseCase() } returns TestMyUserId
     }
 
     // ------------------------------ 나의 키워드인 경우 ------------------------------
 
     @Test
     fun `나의 키워드인 경우 - 초기 데이터인 키워드 상세 정보를 정상적으로 조회한다`() {
+        // given
+        every {
+            usecase.getKeywordDetail(TestMyUserId.value, TestMyUserKeywordId.value)
+        } returns flowOf(Result.Success(TestMyKeywordDetail))
+        savedStateHandle = TestMySavedStateHandle
+        viewModel = KeywordDetailViewModel(
+            usecase = usecase,
+            getUserIdUseCase = getUserIdUseCase,
+            snackbarController = snackbarController,
+            savedStateHandle = savedStateHandle,
+        )
+
+        // when, then
+        testState(
+            viewModel = viewModel,
+            intents = emptyList(),
+            assertions = listOf(
+                KeywordDetailContract.UiState(
+                    myKeyword = true,
+                    keywordDetail = TestMyKeywordDetail.toUiModel(),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `나의 키워드인 경우 - 초기 데이터 조회 시 에러가 발생하면 상태를 초기화하고 스낵바를 표시한다`() = runTest {
+        // given
+        val expectedError = KeywordDetailErrorType.UserIdNotFound
+        every {
+            usecase.getKeywordDetail(TestMyUserId.value, TestMyUserKeywordId.value)
+        } returns flowOf(Result.Error(expectedError))
+        savedStateHandle = TestMySavedStateHandle
+        viewModel = KeywordDetailViewModel(
+            usecase = usecase,
+            getUserIdUseCase = getUserIdUseCase,
+            snackbarController = snackbarController,
+            savedStateHandle = savedStateHandle,
+        )
+
+        val snackbarList = mutableListOf<SnackbarEvent>()
+        val snackbarJob = launch {
+            snackbarController.events.toList(snackbarList)
+        }
+
+        // when, then
+        testState(
+            viewModel = viewModel,
+            intents = emptyList(),
+            assertions = listOf(
+                KeywordDetailContract.UiState(
+                    myKeyword = true,
+                    loading = false,
+                ),
+            ),
+        )
+
+        // then: 스낵바 이벤트 검증
+        assertTrue(snackbarList.isNotEmpty())
+        assertEquals(expectedError.asUiText(), snackbarList.last().message)
+
+        // clean up
+        snackbarJob.cancel()
+    }
+
+    @Test
+    fun `나의 키워드인 경우 - 삭제 성공 시 상태를 초기화하고 스낵바 표시 후 화면을 닫는 일회성 이벤트를 발행한다`() = runTest {
+        // given
+        every {
+            usecase.getKeywordDetail(TestMyUserId.value, TestMyUserKeywordId.value)
+        } returns flowOf(Result.Success(TestMyKeywordDetail))
+        every {
+            usecase.deleteKeyword(TestMyUserKeywordId.value)
+        } returns flowOf(Result.Success(Unit))
+        savedStateHandle = TestMySavedStateHandle
+        viewModel = KeywordDetailViewModel(
+            usecase = usecase,
+            getUserIdUseCase = getUserIdUseCase,
+            snackbarController = snackbarController,
+            savedStateHandle = savedStateHandle,
+        )
+
+        val snackbarList = mutableListOf<SnackbarEvent>()
+        val snackbarJob = launch {
+            snackbarController.events.toList(snackbarList)
+        }
+
+        // when: 키워드 삭제를 진행한다.
+        // then: 로딩 상태 초기화 및 화면을 닫는 일회성 이벤트가 발행된다.
+        testAll(
+            viewModel = viewModel,
+            intents = listOf(
+                KeywordDetailContract.UiEvent.OnDelete,
+            ),
+            assertionStates = listOf(
+                KeywordDetailContract.UiState(
+                    myKeyword = true,
+                    fullScreenLoading = false,
+                    keywordDetail = TestMyKeywordDetail.toUiModel(),
+                ),
+            ),
+            assertionEffects = listOf(
+                KeywordDetailContract.UiEffect.CloseScreen,
+            ),
+        )
+
+        // then: 스낵바 이벤트 검증
+        assertEquals(
+            UiText.StringResource(R.string.keyword_detail_success_delete_keyword),
+            snackbarList.last().message,
+        )
+
+        // clean up
+        snackbarJob.cancel()
+    }
+
+    @Test
+    fun `나의 키워드인 경우 - 삭제 실패 시 상태를 초기화하고 스낵바를 표시한다`() = runTest {
+        // given
+        val expectedError = KeywordDetailErrorType.Unexpected(null)
+        every {
+            usecase.getKeywordDetail(TestMyUserId.value, TestMyUserKeywordId.value)
+        } returns flowOf(Result.Success(TestMyKeywordDetail))
+        every {
+            usecase.deleteKeyword(TestMyUserKeywordId.value)
+        } returns flowOf(Result.Error(expectedError))
+        savedStateHandle = TestMySavedStateHandle
+        viewModel = KeywordDetailViewModel(
+            usecase = usecase,
+            getUserIdUseCase = getUserIdUseCase,
+            snackbarController = snackbarController,
+            savedStateHandle = savedStateHandle,
+        )
+
+        val snackbarList = mutableListOf<SnackbarEvent>()
+        val snackbarJob = launch {
+            snackbarController.events.toList(snackbarList)
+        }
+
+        // when: 키워드 삭제를 진행한다.
+        // then: 로딩 상태 초기화 및 화면을 닫는 일회성 이벤트가 발행된다.
+        testState(
+            viewModel = viewModel,
+            intents = listOf(
+                KeywordDetailContract.UiEvent.OnDelete,
+            ),
+            assertions = listOf(
+                KeywordDetailContract.UiState(
+                    myKeyword = true,
+                    fullScreenLoading = false,
+                    keywordDetail = TestMyKeywordDetail.toUiModel(),
+                ),
+            ),
+        )
+
+        // then: 스낵바 이벤트 검증
+        assertEquals(expectedError.asUiText(), snackbarList.last().message)
+
+        // clean up
+        snackbarJob.cancel()
+    }
+
+    // ------------------------------ 사용자 키워드인 경우 ------------------------------
+
+    @Test
+    fun `사용자 키워드인 경우 - 초기 데이터인 키워드 상세 정보를 정상적으로 조회한다`() {
         // given
         every {
             usecase.getKeywordDetail(TestUserId.value, TestUserKeywordId.value)
@@ -74,7 +238,7 @@ class KeywordDetailViewModelTest : MVIBaseViewModelTest<
             intents = emptyList(),
             assertions = listOf(
                 KeywordDetailContract.UiState(
-                    myKeyword = true,
+                    myKeyword = false,
                     keywordDetail = TestKeywordDetail.toUiModel(),
                 ),
             ),
@@ -82,7 +246,7 @@ class KeywordDetailViewModelTest : MVIBaseViewModelTest<
     }
 
     @Test
-    fun `나의 키워드인 경우 - 초기 데이터 조회 시 에러가 발생하면 정상적으로 에러를 표시한다`() = runTest {
+    fun `사용자 키워드인 경우 - 초기 데이터 조회 시 에러가 발생하면 상태를 초기화하고 스낵바를 표시한다`() = runTest {
         // given
         val expectedError = KeywordDetailErrorType.UserIdNotFound
         every {
@@ -107,77 +271,8 @@ class KeywordDetailViewModelTest : MVIBaseViewModelTest<
             intents = emptyList(),
             assertions = listOf(
                 KeywordDetailContract.UiState(
-                    myKeyword = true,
-                    error = expectedError.asUiText(),
-                ),
-            ),
-        )
-
-        // then: 스낵바 이벤트 검증
-        assertTrue(snackbarList.isNotEmpty())
-        assertEquals(expectedError.asUiText(), snackbarList.last().message)
-
-        // clean up
-        snackbarJob.cancel()
-    }
-
-    // ------------------------------ 사용자 키워드인 경우 ------------------------------
-
-    @Test
-    fun `사용자 키워드인 경우 - 초기 데이터인 키워드 상세 정보를 정상적으로 조회한다`() {
-        // given
-        every {
-            usecase.getKeywordDetail(TestOtherUserId.value, TestOtherUserKeywordId.value)
-        } returns flowOf(Result.Success(TestOtherKeywordDetail))
-        savedStateHandle = TestOtherSavedStateHandle
-        viewModel = KeywordDetailViewModel(
-            usecase = usecase,
-            getUserIdUseCase = getUserIdUseCase,
-            snackbarController = snackbarController,
-            savedStateHandle = savedStateHandle,
-        )
-
-        // when, then
-        testState(
-            viewModel = viewModel,
-            intents = emptyList(),
-            assertions = listOf(
-                KeywordDetailContract.UiState(
                     myKeyword = false,
-                    keywordDetail = TestOtherKeywordDetail.toUiModel(),
-                ),
-            ),
-        )
-    }
-
-    @Test
-    fun `사용자 키워드인 경우 - 초기 데이터 조회 시 에러가 발생하면 정상적으로 에러를 표시한다`() = runTest {
-        // given
-        val expectedError = KeywordDetailErrorType.UserIdNotFound
-        every {
-            usecase.getKeywordDetail(TestOtherUserId.value, TestOtherUserKeywordId.value)
-        } returns flowOf(Result.Error(expectedError))
-        savedStateHandle = TestOtherSavedStateHandle
-        viewModel = KeywordDetailViewModel(
-            usecase = usecase,
-            getUserIdUseCase = getUserIdUseCase,
-            snackbarController = snackbarController,
-            savedStateHandle = savedStateHandle,
-        )
-
-        val snackbarList = mutableListOf<SnackbarEvent>()
-        val snackbarJob = launch {
-            snackbarController.events.toList(snackbarList)
-        }
-
-        // when, then
-        testState(
-            viewModel = viewModel,
-            intents = emptyList(),
-            assertions = listOf(
-                KeywordDetailContract.UiState(
-                    myKeyword = false,
-                    error = expectedError.asUiText(),
+                    loading = false,
                 ),
             ),
         )
@@ -192,8 +287,29 @@ class KeywordDetailViewModelTest : MVIBaseViewModelTest<
 
     companion object {
         // ------------------------------ 나의 키워드 테스트 데이터 ------------------------------
-        private val TestUserId = UserId(1L)
-        private val TestUserKeywordId = UserKeywordId(1L)
+        private val TestMyUserId = UserId(1L)
+        private val TestMyUserKeywordId = UserKeywordId(1L)
+        private val TestMySavedStateHandle = SavedStateHandle(
+            mapOf(
+                "userId" to TestMyUserId.value,
+                "userKeywordId" to TestMyUserKeywordId.value,
+            ),
+        )
+        private val TestMyKeywordDetail = KeywordDetail(
+            userKeywordId = TestMyUserKeywordId,
+            keywordId = KeywordId(1L),
+            keyword = KeywordName("k_name"),
+            description = KeywordDescription("desc"),
+            userId = TestMyUserId,
+            userName = Name("name"),
+            profileImageUrl = null,
+            createdAt = 0L,
+            updatedAt = 0L,
+        )
+
+        // ------------------------------ 사용자 키워드 테스트 데이터 ------------------------------
+        private val TestUserId = UserId(100L)
+        private val TestUserKeywordId = UserKeywordId(100L)
         private val TestSavedStateHandle = SavedStateHandle(
             mapOf(
                 "userId" to TestUserId.value,
@@ -206,27 +322,6 @@ class KeywordDetailViewModelTest : MVIBaseViewModelTest<
             keyword = KeywordName("k_name"),
             description = KeywordDescription("desc"),
             userId = TestUserId,
-            userName = Name("name"),
-            profileImageUrl = null,
-            createdAt = 0L,
-            updatedAt = 0L,
-        )
-
-        // ------------------------------ 사용자 키워드 테스트 데이터 ------------------------------
-        private val TestOtherUserId = UserId(100L)
-        private val TestOtherUserKeywordId = UserKeywordId(100L)
-        private val TestOtherSavedStateHandle = SavedStateHandle(
-            mapOf(
-                "userId" to TestOtherUserId.value,
-                "userKeywordId" to TestOtherUserKeywordId.value,
-            ),
-        )
-        private val TestOtherKeywordDetail = KeywordDetail(
-            userKeywordId = TestOtherUserKeywordId,
-            keywordId = KeywordId(1L),
-            keyword = KeywordName("k_name"),
-            description = KeywordDescription("desc"),
-            userId = TestOtherUserId,
             userName = Name("name"),
             profileImageUrl = null,
             createdAt = 0L,
