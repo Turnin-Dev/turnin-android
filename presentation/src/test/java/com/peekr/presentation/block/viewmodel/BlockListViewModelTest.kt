94234df2d787362a7a599de3ac487bacd703a4c7
DDangDin <Kcorelec7123@gmail.com>
2026-02-18 23:32:28 +0900

    [PK-105] refactor: 코드 리뷰 반영
    - 불필요한 import 제거
    - BlockListViewModelTest: 차단 해제 테스트 시 advanceUntilIdle() 추가

--- presentation/src/test/java/com/peekr/presentation/block/viewmodel/BlockListViewModelTest.kt
+++ presentation/src/test/java/com/peekr/presentation/block/viewmodel/BlockListViewModelTest.kt
@@ -122,7 +122,7 @@ class BlockListViewModelTest {
 
         // then: 페이징 데이터에서 차단 ID가 1인 데이터를 제외한 나머지 데이터가 반환된다.
         assertEquals(expectedList.size - 1, actualList.size)
-        assertEquals(expectedList.filter { it.id!=1L }, actualList)
+        assertEquals(expectedList.filter { it.id != 1L }, actualList)
     }
 
     @Test
