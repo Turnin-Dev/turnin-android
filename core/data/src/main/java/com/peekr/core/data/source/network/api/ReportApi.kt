84d86bbd7bc9f4b41f753a20975a4cb831b1ccf0
DDangDin <Kcorelec7123@gmail.com>
2026-04-23 18:49:33 +0900

    feat: 신고/차단 사유 목록 조회 API에 HTTP 캐시 적용

--- core/data/src/main/java/com/peekr/core/data/source/network/api/ReportApi.kt
+++ core/data/src/main/java/com/peekr/core/data/source/network/api/ReportApi.kt
@@ -2,6 +2,8 @@ package com.peekr.core.data.source.network.api
 
 import com.peekr.core.data.source.network.dto.report.request.ReportRequest
 import com.peekr.core.data.source.network.dto.report.response.ReportReasonsResponse
+import com.peekr.core.data.source.network.retrofit.Cacheable
+import com.peekr.core.data.source.network.retrofit.HttpCacheDuration
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -10,6 +12,7 @@ import retrofit2.http.POST
 /** Report Network API */
 interface ReportApi {
     /** 신고 사유 목록 조회 */
+    @Cacheable(maxAge = HttpCacheDuration.ONE_WEEK)
     @GET(NetworkApiPath.Report.REASON)
     suspend fun getReportReasons(): Response<ReportReasonsResponse>
 
