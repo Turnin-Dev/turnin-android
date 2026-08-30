58f2fdf6604d97f9394b85a6366e54207b47ef1c
DDangDin <Kcorelec7123@gmail.com>
2026-08-30 17:18:25 +0900

    build: firebase performance monitoring 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.gms) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
 }
 
 apply(from = "ktlint.gradle.kts")
